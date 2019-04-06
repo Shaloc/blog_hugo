b4c14c3c9372f3acc9c1c6b468fd18164a24c2e1
Shaloc <shalocn@outlook.com>
2019-04-06 20:05:44 +0800

    update git login details

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,11 +3,12 @@
 set -ev
 export TZ='Asia/Shanghai'
 
-git clone -b master https://github.com/Shaloc/shaloc.github.io .deploy_git
+git clone -b master git@github.com/Shaloc/shaloc.github.io .deploy_git
 cd .deploy_git
 git checkout master
 mv .git/ ../public/
 cd ../public
-git add -A
+
+git add .
 git commit -m "Site updated: $(date +"%Y-%m-%d %H:%M:%S")"
-git push "https://github.com/Shaloc/shaloc.github.io" master:master --force --quiet
+git push origin master:master --force --quiet
