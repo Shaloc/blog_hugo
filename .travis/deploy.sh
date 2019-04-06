454f7f27f6697d640f065ee6da6a0ea94965bbc0
Shaloc <shalocn@outlook.com>
2019-04-06 18:56:06 +0800

    add another way to deploy keys

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,6 @@
 
 set -ev
 export TZ='Asia/Shanghai'
-G='e3ff32d5bd875634126822a70e07d95e9fe38e59'
 
 git clone -b master https://github.com/Shaloc/shaloc.github.io .deploy_git
 cd .deploy_git
@@ -11,4 +10,4 @@ mv .git/ ../public/
 cd ../public
 git add -A
 git commit -m "Site updated: $(date +"%Y-%m-%d %H:%M:%S")"
-git push "https://Shaloc:${G}@github.com/Shaloc/shaloc.github.io" master:master --force --quiet
+git push "https://github.com/Shaloc/shaloc.github.io" master:master --force --quiet
