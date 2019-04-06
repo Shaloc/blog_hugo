ba853e278d0eedc4721ed51e518f011c4d74cb22
Shaloc <shalocn@outlook.com>
2019-04-06 17:27:14 +0800

    update script

---
+++ .travis/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ev
+export TZ='Asia/Shanghai'
+
+git clone -b master https://github.com/Shaloc/shaloc.github.io .deploy_git
+cd .deploy_git
+git checkout master
+mv .git/ ../public/
+cd ../public
+git add -A
+git commit -m "Site updated: 'date +"%Y-%m-%d %H:%M:%S"'"
+git push origin master:master --force --quiet
