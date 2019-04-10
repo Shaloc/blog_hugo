60abfe9b54de6ed307989476f267b064142e3acc
Shaloc <shalocn@outlook.com>
2019-04-10 17:26:58 +0800

    add backup in nju git

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -14,6 +14,8 @@ mv CNAME ../public/
 
 # copy files and commit/push
 cd ../public
+git remote add nju git@git.nju.edu.cn:Shaloc/shaloc.github.io.git
 git add .
 git commit -m "Site updated: $(date +"%Y-%m-%d %H:%M:%S")"
 git push origin master:master --force --quiet
+git push nju master:master --force --quiet
